0136f90da08c8f7c301fd80e6b8d6dfd8e72871c
kuzdogan <kaanuzdogan@hotmail.com>
2019-10-08 16:42:07 +0200

    Vouch method test

--- test/testReviewStorage.js
+++ test/testReviewStorage.js
@@ -17,7 +17,25 @@ contract("ReviewStorage test", async accounts => {
     let reviewArr = [journalId, manuscriptId, manuscriptHash, web3.utils.toBN(timeStamp),
        web3.utils.toBN(recommendation), verified, vouchers];
     let reviewObj = Object.assign({}, reviewArr);
-    console.log(addedReview);
     assert.deepEqual(addedReview, reviewObj, 'Contract response does not match expected values')
   });
+
+  it("should vouch a review", async () => {
+    let accounts = await web3.eth.getAccounts();
+    let author = accounts[0];
+    let index = 0; // Index of the review.
+    let voucher1 = accounts[1];
+    console.log(voucher1);
+    let voucher2 = accounts[2];
+
+    let instance = await ReviewStorage.deployed();
+    await instance.vouch(author, index, { // Vouch first Review of the first account.
+      from: voucher1
+    });
+
+    let review = await instance.getReview(author, index);
+    let vouchers = review[6];
+    let expectedVouchers = [voucher1];
+    assert.deepEqual(vouchers, expectedVouchers, 'Vouchers of review not as expected')
+  })
 });
