254bd7af7c68d7455beb0a9af58b2534bc11e24a
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2019-10-29 13:06:44 +0100

    Successful add review request from client

--- client/src/components/Reviews/Reviews-container.jsx
+++ client/src/components/Reviews/Reviews-container.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Review from './Reviews-view';
 
+const URL = 'http://localhost:3000';
+
 export default class ReviewsContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -11,8 +13,24 @@ export default class ReviewsContainer extends React.Component {
         'manuscriptHash': '',
         'timestamp': new Date(),
         'recommendation': null
-      }
+      },
+      account: '0xb454b9e3fB8307AE28d2E0243c5e99A47236a2e0'
     };
+  }
+  sendPostRequestToEndpoint = (URL, endpoint, body) => {
+    console.log(body);
+    return fetch(URL + endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+  }
+
+  addReview = (data) => {
+    let account = this.state.account;
+    return (this.sendPostRequestToEndpoint(URL, `/users/${account}/reviews`, data));
   }
 
   handleDateChange = (date) => {
@@ -25,6 +43,7 @@ export default class ReviewsContainer extends React.Component {
 
   handleSubmit = (data) => {
     console.log(data);
+    this.addReview(data).then(res => console.log(res)).catch(err => console.log(err));
   }
   render() {
     return (
