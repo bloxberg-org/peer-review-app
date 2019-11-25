6af587d5816e6273828674ec709c8038652aeb38
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2019-11-25 10:29:51 +0100

    Added config template

---
+++ server/config_template.js
@@ -0,0 +1,9 @@
+// Edit and name this file as config.js
+
+exports.database = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'test' // change db name accordingly
+};
+
+exports.databaseURI = 'ENTER DB URI, WITH USER AND PASSWORD IF EXISTS';
