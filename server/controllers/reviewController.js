976f0de20b14a80b51b3985a75222d043462b3d8
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2019-11-26 17:08:02 +0100

    Refactor

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -71,90 +71,110 @@ exports.getReviewXML = (req, res) => {
   getXML(`${URL}${doi}`).then((data) => {
     if (!data)
       res.status(404).send('XML is empty');
-
     // Parse XML to JSON
     let jsonText = xml2json(data, { compact: true, spaces: 2 }); // convert to json
     let jsonDoc = JSON.parse(jsonText);
-
-    // If the specific review tied to this article is requested, just send the reivew information.
-    // Else, first ask user which review to return 
+    // If the specific review tied to this article is requested, send the review information.
+    // Else, first send the list of reviews of this article to ask user which review to return 
     if (index) {
-      console.log('REQUESTED REVIEW WITH ID ' + index);
-      // Don't send the whole json, not needed. Filter out required fields.
-      let reviewDoc = jsonDoc['article']['sub-article'][index];
-      let articleMeta = jsonDoc['article']['front']['article-meta'];
-      let journalMeta = jsonDoc['article']['front']['journal-meta'];
-      let reviewBody = reviewDoc['body'];
-
-      // Extract the review content from the converted json file. reviewBody object is of the following format:
-      // 
-      // p: [
-      //   {
-      //     _text: 'This is a well selected review that covers most of the essential 2018 works on Tourette syndrome.'
-      //   },
-      //   {
-      //     _text: ' These measures are related to SMA and basal ganglia functioning which is pretty consistent with neuroimaging and electrophysio....
-      // 
-      // So we reduce the array under the field 'p'.
-      // First set an empty string to be the initial value of the accumulator.
-      // Then accumulate _text fields of each subsequent element , with a new line, into a single string.
-      let reviewContent = reviewBody['p'].reduce((accumulator, currentVal) => {
-        // TODO: Handle Arrays. We assume just one level of depth and Array of strings get concatenated via toString() automatically. This leaves emptry strings in the text for some reason.
-        return accumulator + currentVal['_text'] + '\n';
-      }, '');
-
-      let reviewRecommendation = (() => {
-        let reccommendationStr = reviewDoc['front-stub']['custom-meta-group']['custom-meta']['meta-value']['_text'];
-        // TODO: Check returned strings for review and reject.
-        // TODO: Change strings to numbers in accordance to the blockchain.
-        console.log(reccommendationStr);
-        switch (reccommendationStr) {
-          case ('approve'):
-            return 0;
-          case ('review'):
-            return 1;
-          case ('reject'):
-            return 2;
-        }
-      })(); // () to invoke immediately.
-
-      let reviewPubDate = reviewDoc['front-stub']['pub-date']; // JSON object with day, month, year.
-      let reviewPubTimestamp = moment(`${reviewPubDate['year']['_text']} ${reviewPubDate['month']['_text']} ${reviewPubDate['day']['_text']}`, 'YYYY MM DD');
-      let response = {
-        articleTitle: articleMeta['title-group']['article-title']['_text'],
-        journalId: journalMeta['issn']['_text'],
-        manuscriptID: articleMeta['article-id']['_text'],
-        manuscriptHash: null,
-        articleDOI: articleMeta['article-id']['_text'],
-        // TODO: Check for other formats of recommentdation.
-        recommendation: reviewRecommendation,
-        timestamp: reviewPubTimestamp,
-        content: reviewContent
-      };
+      let response = extractReviewFromJsonDoc(jsonDoc, index);
       res.status(200).json(response);
     } else {
-      console.log('ASKING USER TO CHOOSE A REVIEW');
-      let reviews = jsonDoc['article']['sub-article'];
-      reviews = reviews.map(review => {
-        return {
-          author: {
-            firstName: review['front-stub']['contrib-group']['contrib']['name']['given-names']['_text'],
-            lastName: review['front-stub']['contrib-group']['contrib']['name']['surname']['_text'],
-            uri: review['front-stub']['contrib-group']['contrib']['uri']['_text'],
-          },
-          date: {
-            day: review['front-stub']['pub-date']['day']['_text'],
-            month: review['front-stub']['pub-date']['month']['_text'],
-            year: review['front-stub']['pub-date']['year']['_text']
-          }
-        };
-      });
+      let reviews = extractListOfReviews(jsonDoc);
       res.status(200).json(reviews);
-
     }
   }).catch(err => res.status(500).send(err));
 };
 
+function extractReviewFromJsonDoc(jsonDoc, index) {
+  console.log('REQUESTED REVIEW WITH ID ' + index);
+  // Relevant fields of the whole json
+  let reviewDoc = jsonDoc['article']['sub-article'][index];
+  let articleMeta = jsonDoc['article']['front']['article-meta'];
+  let journalMeta = jsonDoc['article']['front']['journal-meta'];
+  let reviewBody = reviewDoc['body'];
+
+  let response = {
+    articleTitle: articleMeta['title-group']['article-title']['_text'],
+    journalId: journalMeta['issn']['_text'],
+    manuscriptId: articleMeta['article-id']['_text'],
+    manuscriptHash: null,
+    articleDOI: articleMeta['article-id']['_text'],
+    // TODO: Check for other formats of recommentdation.
+    recommendation: extractRecommendation(reviewDoc),
+    timestamp: extractReviewTimestamp(reviewDoc),
+    content: extractReviewBody(reviewBody)
+  };
+  return response;
+}
+
+function extractListOfReviews(jsonDoc) {
+  console.log('ASKING USER TO CHOOSE A REVIEW');
+  let reviews = jsonDoc['article']['sub-article']; // Returns an array of reviews when there are 2 or more reivews.
+  // TODO: What does it return when there's only 1 review? 
+
+  // Just format the response 
+  reviews = reviews.map(review => {
+    return {
+      author: {
+        firstName: review['front-stub']['contrib-group']['contrib']['name']['given-names']['_text'],
+        lastName: review['front-stub']['contrib-group']['contrib']['name']['surname']['_text'],
+        uri: review['front-stub']['contrib-group']['contrib']['uri']['_text'],
+      },
+      date: {
+        day: review['front-stub']['pub-date']['day']['_text'],
+        month: review['front-stub']['pub-date']['month']['_text'],
+        year: review['front-stub']['pub-date']['year']['_text']
+      }
+    };
+  });
+  return reviews;
+}
+
+function extractReviewBody(reviewBody) {
+
+  // Extract the review content from the converted json file. 
+  // reviewBody object is of the following format:
+  // 
+  // p: [
+  //   {
+  //     _text: 'This is a well selected review that covers most of the essential 2018 works on Tourette syndrome.'
+  //   },
+  //   {
+  //     _text: ' These measures are related to SMA and basal ganglia functioning which is pretty consistent with neuroimaging and electrophysio....
+  // 
+  // We need the content of the review text under p.
+  // So we reduce the array under the field 'p'.
+  // First set an empty string to be the initial value of the accumulator.
+  // Then accumulate _text fields of each subsequent element , with a new line, into a single string.
+  let reviewContent = reviewBody['p'].reduce((accumulator, currentVal) => {
+    // TODO: Handle Arrays. We assume just one level of depth and Array of strings get concatenated via toString() automatically. This leaves emptry strings in the text for some reason.
+    return accumulator + currentVal['_text'] + '\n';
+  }, '');
+
+  return reviewContent;
+}
+
+function extractRecommendation(reviewDoc) {
+  let reccommendationStr = reviewDoc['front-stub']['custom-meta-group']['custom-meta']['meta-value']['_text'];
+  // TODO: Check returned strings for review and reject.
+  // TODO: Change strings to numbers in accordance to the blockchain.
+  console.log(reccommendationStr);
+  switch (reccommendationStr) {
+    case ('approve'):
+      return 0;
+    case ('review'):
+      return 1;
+    case ('reject'):
+      return 2;
+  }
+}
+
+function extractReviewTimestamp(reviewDoc) {
+  let reviewPubDate = reviewDoc['front-stub']['pub-date']; // JSON object with day, month, year.
+  let reviewPubTimestamp = moment(`${reviewPubDate['year']['_text']} ${reviewPubDate['month']['_text']} ${reviewPubDate['day']['_text']}`, 'YYYY MM DD');
+  return reviewPubTimestamp;
+}
 // exports.getReview = async (req, res) => {
 //   let review;
 //   try {
@@ -188,3 +208,4 @@ exports.getReviewXML = (req, res) => {
 //     res.status(500).send(e);
 //   }
 // };
+
