64e203eb4b5c19d6bafeffc25465fd11bc5eeab0
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2019-12-04 09:36:06 +0100

    Config for deployign to bloxberg

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,37 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
+const mnemonic = process.env.MNEMONIC;
+
+// Provider engine instantiation can be included but is not necessary when account is unlocked
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  // contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    bloxberg: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, 'https://core.bloxberg.org'),
+      network_id: '8995',
+      gas: 8000000,   // <--- Twice as much
+      gasPrice: 4 * 1000000000, // X * gwei's
+      from: '0xAA6042aa65eb93C6439cDaeBC27B3bd09c5DFe94'
+    },
+    bloxbergDev: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, 'https://bloxberg.org/eth/'),
+      network_id: '8995',
+      from: '0xAA6042aa65eb93C6439cDaeBC27B3bd09c5DFe94'
+    }
+  },
+  compilers: {
+    solc: {
+      version: '^0.5.0',
+      settings: {
+        evmVersion: 'byzantium' // Default: "petersburg"
+      }
     }
   }
-};
+}
