8d4f829df27f7722922fc2dcf2c9863d72aad286
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2020-02-17 17:13:00 +0100

    Send firstName, lastName to database. Send Eth address as _id.

--- client/src/components/Register/Register-view.jsx
+++ client/src/components/Register/Register-view.jsx
@@ -1,13 +1,12 @@
-import { getCurrentAccount } from  '../../connection/reviewConnection';
 import React, { useState } from 'react';
 import useForm from 'react-hook-form';
-import FormField from '../FormField';
-import Button from '../Button';
-import styled from 'styled-components';
-import { addScholar } from '../../utils/register';
-import uniqid from 'uniqid';
-import CardWrapper from '../CardWrapper';
 import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import { getCurrentAccount } from '../../connection/reviewConnection';
+import { addScholar } from '../../utils/register';
+import Button from '../Button';
+import CardWrapper from '../CardWrapper';
+import FormField from '../FormField';
 
 const Wrapper = styled.div`
   display: flex;
@@ -32,13 +31,8 @@ export default function App() {
   const { register, handleSubmit, errors } = useForm();
   let history = useHistory();
 
-  const onSubmit = data => {
-    console.log(data);
-
-    let scholar = {
-      id: uniqid(),
-      ...data
-    };
+  const onSubmit = scholar => {
+    console.log(scholar);
 
     addScholar(scholar)
       .then((response) => {
@@ -64,31 +58,31 @@ export default function App() {
             <FormField
               type="text"
               placeholder="First name"
-              name="name"
-              title="Name"
-              errors={errors.name}
-              register={register({required: true, maxLength: 80})} />
+              name="firstName"
+              title="First Name"
+              errors={errors.firstName}
+              register={register({ required: true, maxLength: 80 })} />
             <FormField
               type="text"
-              placeholder="Last name"
-              name="surname"
+              placeholder="Last Name"
+              name="lastName"
               title="Surname"
-              errors={errors.title}
-              register={register({required: true, maxLength: 100})} />
+              errors={errors.lastName}
+              register={register({ required: true, maxLength: 100 })} />
             <FormField
               type="text"
-              placeholder="Email"
+              placeholder="E-mail"
               name="email"
               title="Email"
               errors={errors.email}
-              register={register({required: true, pattern: /^\S+@\S+$/i})} />
+              register={register({ required: true, pattern: /^\S+@\S+$/i })} />
             <FormField
               type="text"
               placeholder="0x...."
               name="_id"
               title="Ethereum Address"
               errors={errors._id}
-              register={register({required: true})} value={account} disabled />
+              register={register({ required: true })} value={account} disabled />
             <ButtonWrapper>
               <Button primary>Register</Button>
             </ButtonWrapper>
--- client/src/utils/register.js
+++ client/src/utils/register.js
@@ -11,7 +11,6 @@ export const addScholar = (data) => {
   return getCurrentAccount().then((address) => {
     // Prepare DB data.
     let dbData = {
-      //_id: address,
       ...data,
       reviews: []
     };
