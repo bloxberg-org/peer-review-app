c09e1d38b393a23f2a4ac89a6222e858a9569e9f
kuzdogan <kaanuzdogan@hotmail.com>
2020-03-30 16:52:02 +0200

    Enable test network in App-container. Deploy to test network, ABIs changed.

--- truffle-config.js
+++ truffle-config.js
@@ -12,7 +12,7 @@ module.exports = {
     develop: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "5777" // Match any network id
     },
     bloxberg: {
       provider: bloxbergProvider,
