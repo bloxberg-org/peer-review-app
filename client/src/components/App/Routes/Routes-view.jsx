106a14373ac7a21300e716a97c70c20eb57c19e1
kuzdogan <kaanuzdogan@hotmail.com>
2020-04-16 10:19:41 +0200

    Remove unnecessary Switches

--- client/src/components/App/Routes/Routes-view.jsx
+++ client/src/components/App/Routes/Routes-view.jsx
@@ -7,7 +7,6 @@ import Overview from '../../Overview';
 import SingleReview from '../../SingleReview';
 import TopBar from '../../TopBar';
 import VouchReview from '../../VouchReview';
-
 export default function RoutesView(props) {
   return (
     <Switch>
@@ -15,36 +14,32 @@ export default function RoutesView(props) {
         <TopBar title='Overview' {...props} />
         <Overview {...props} />
       </Route>
-      <Route path="/Reviews">
-        <Switch>
-          <Route path="/Reviews/AddReview">
-            <TopBar title='Reviews' {...props} />
-            <AddReview {...props} />
-          </Route>
-          <Route path="/Reviews/ImportReviews">
-            <TopBar title='Reviews' {...props} />
-            <ImportReviews {...props} />
-          </Route>
-          <Route path="/Reviews/YourReviews">
-            <TopBar title='Reviews' {...props} />
-            <AllReviews {...props} />
-          </Route>
-          <Route path="/Reviews/VouchReview">
-            <TopBar title='Reviews' {...props} />
-            <VouchReview {...props} />
-          </Route>
-          <Route path="/Reviews/:id">
-            <TopBar title='Review' {...props} />
-            <SingleReview {...props} />
-          </Route>
-
-          {/* Redirect to AddReview at route /Review/ */}
-          <Redirect to='/Reviews/AddReview' />
-        </Switch>
+      <Route path="/Reviews/AddReview">
+        <TopBar title='Reviews' {...props} />
+        <AddReview {...props} />
       </Route>
+      <Route path="/Reviews/ImportReviews">
+        <TopBar title='Reviews' {...props} />
+        <ImportReviews {...props} />
+      </Route>
+      <Route path="/Reviews/YourReviews">
+        <TopBar title='Reviews' {...props} />
+        <AllReviews {...props} />
+      </Route>
+      <Route path="/Reviews/VouchReview">
+        <TopBar title='Reviews' {...props} />
+        <VouchReview {...props} />
+      </Route>
+      <Route path="/Reviews/:id">
+        <TopBar title='Review' {...props} />
+        <SingleReview {...props} />
+      </Route>
+
+      {/* Redirect to AddReview at route /Review/ */}
+      <Redirect to='/Reviews/AddReview' />
       <Route path="/">
         <Redirect to="/Overview" />
       </Route>
     </Switch>
-  )
+  );
 }
