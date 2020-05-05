0f9d9887fa0a110b202d256f033aa50afc597143
kuzdogan <kaanuzdogan@hotmail.com>
2020-05-05 14:51:49 +0200

    Show AddReview options as radio buttons and in the same view

--- client/src/components/AddReview/F1000R/F1000R-view.jsx
+++ client/src/components/AddReview/F1000R/F1000R-view.jsx
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'styled-components';
-import CardWrapper from '../../CardWrapper';
 import Loader from '../../Loader';
 import AddReviewForm from './AddReviewForm';
 import DOIInput from './DOIInput';
@@ -18,11 +16,6 @@ F1000RView.propTypes = {
 // ================ Define Wrappers =================
 //===================================================
 
-const Wrapper = styled.div`
-  display: flex;
-  flex: 1
-  `;
-
 export default function F1000RView(props) {
 
 
@@ -33,21 +26,13 @@ export default function F1000RView(props) {
     );
   }
 
-  return (
-    <Wrapper id='wrapper'>
-      <CardWrapper title='Add a Review from F1000R'>
-        {
-          !props.review // Show DOI input if review is not yet retrieved
-            ?
-            <DOIInput
-              handleReview={props.handleReview} />
-            :
-            <AddReviewForm
-              handleDateChange={props.handleDateChange}
-              handleSubmit={props.handleSubmit}
-              review={props.review} />
-        }
-      </CardWrapper>
-    </Wrapper>
-  );
+  if (!props.review) { // Show DOI input if review is not yet retrieved
+    return <DOIInput
+      handleReview={props.handleReview} />;
+  } else {
+    return <AddReviewForm
+      handleDateChange={props.handleDateChange}
+      handleSubmit={props.handleSubmit}
+      review={props.review} />;
+  }
 }
